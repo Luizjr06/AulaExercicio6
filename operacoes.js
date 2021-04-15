066d79e384f539675dec4c040fd978a24c0b4a24
Luiz <luizjr06@hotmail.com>
2021-04-14 23:28:49 -0300

    Aula06

---
+++ operacoes.js
@@ -0,0 +1,16 @@
+module.exports={
+    calcular(n1,n2,op){
+
+   if(op=== "+"){
+       return n1+n2
+   }else if(op==="-"){
+       return n1-n2
+   }else if (op === "/"){
+       return n1/n2
+   }else if(op==="*"){
+       return n1*n2
+   }else{
+       return "Operação inesperada"
+   }
+}
+};
